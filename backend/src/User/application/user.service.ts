b17581d623f10c0289625dac7a1cf941b5fe136d
VJA <victorj.alonzo@gmail.com>
2025-05-08 23:20:21 -0400

    feat(user): add fetchMany method to fetch users

--- backend/src/User/application/user.service.ts
+++ backend/src/User/application/user.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from "@nestjs/common"
 import { User } from "../domain/user.type.js"
-import { Repository } from "../../Shared/domain/repository.type.js"
 import { UserRepository } from "../infrastructure/user.repository.js"
 import { UserNotFoundException } from "../domain/user.exceptions.js"
 
@@ -9,10 +8,14 @@ export class UserService {
     constructor(private readonly repository: UserRepository){}
 
     findOne = async (id: string): Promise<User> => {
-        return await this.repository.get({ id })
-        .then(r => {
-            if (!r) throw new UserNotFoundException()
-            return r
+        return await this.repository.findOne({ id })
+        .then(user => {
+            if (!user) throw new UserNotFoundException()
+            return user
         })
+    }
+
+    findMany = async (role?: string): Promise<User[]> => {
+        return await this.repository.findMany({role})
     }
 }
