79ef85b23a5bfb9144782781500422e8d2efb2d3
VJA <victorj.alonzo@gmail.com>
2025-05-08 23:23:15 -0400

    feat(user-repository): add fetchMany method to fetch users

--- backend/src/User/infrastructure/user.repository.ts
+++ backend/src/User/infrastructure/user.repository.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { Repository } from '../../Shared/domain/repository.type.js';
 import { PrismaService } from '../../Database/prisma.service.js';
 import { User } from '../domain/user.type.js';
 
 @Injectable()
-export class UserRepository implements Repository<User> {
+export class UserRepository {
     constructor(private prisma: PrismaService) {}
 
-    async get(filters: Record<string, any>): Promise<User | null> {
+    async findOne(filters: Record<string, any>): Promise<User | null> {
         return <User><unknown>this.prisma.user.findFirst({ where: filters })
+    }
+
+    async findMany(filters?: Record<string, any>): Promise<User[]> {
+        return <User[]><unknown>this.prisma.user.findMany({ where: filters })
     }
 }
