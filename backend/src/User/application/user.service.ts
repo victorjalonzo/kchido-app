441fc33e6a4db1ef5b89836c31c1a107204166a4
VJA <victorj.alonzo@gmail.com>
2025-05-09 23:15:51 -0400

    feat(user-service) add method to delete user

--- backend/src/User/application/user.service.ts
+++ backend/src/User/application/user.service.ts
@@ -1,14 +1,17 @@
 import {Injectable} from "@nestjs/common"
 import { User } from "../domain/user.type.js"
-import { UserRepository } from "../infrastructure/user.repository.js"
 import { UserNotFoundException } from "../domain/user.exceptions.js"
+import { SharedRepository } from "../../Shared/shared.repository.js"
+import { Model } from "../../Shared/shared.types.js"
 
 @Injectable()
 export class UserService {
-    constructor(private readonly repository: UserRepository){}
+    model: Model = Model.USERS
+
+    constructor(private readonly repository: SharedRepository<User>){}
 
     findOne = async (id: string): Promise<User> => {
-        return await this.repository.findOne({ id })
+        return await this.repository.findOne(this.model, { id })
         .then(user => {
             if (!user) throw new UserNotFoundException()
             return user
@@ -16,6 +19,14 @@ export class UserService {
     }
 
     findMany = async (role?: string): Promise<User[]> => {
-        return await this.repository.findMany({role})
+        return await this.repository.findMany(this.model, {role})
+    }
+
+    delete = async (id: string): Promise<User> => {
+        return await this.repository.delete(this.model, {id})
+        .then(user => {
+            if (!user) throw new UserNotFoundException()
+            return user
+        })
     }
 }
