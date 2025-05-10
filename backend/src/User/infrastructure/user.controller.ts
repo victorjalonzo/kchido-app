48d829ab94e626095a35ab699e8b93f67ecff37e
VJA <victorj.alonzo@gmail.com>
2025-05-09 23:20:54 -0400

    refactor(user) simplify exception handle

--- backend/src/User/infrastructure/user.controller.ts
+++ backend/src/User/infrastructure/user.controller.ts
@@ -1,33 +1,22 @@
-import {Controller, Get, Param, Res, HttpStatus, Query} from "@nestjs/common"
+import {Controller, Get, Param, Res, HttpStatus, Query, UseFilters} from "@nestjs/common"
 import { UserService } from "../application/user.service.js"
 import { Response } from 'express';
-import { handleUserResponseException } from "./user.util.js";
+import { UserExceptionFilter } from "./user-exception.filter.js";
 
+@UseFilters(UserExceptionFilter)
 @Controller("api/v1/users")
 export class UserController {
     constructor(private readonly userService: UserService){}
 
     @Get()
     async findMany(@Query('role') role: string, @Res() res: Response) {
-        try {
-            const data = await this.userService.findMany(role)
-            return res.status(HttpStatus.OK).json(data)
-        }
-        catch(e) {
-            return handleUserResponseException(res, e)
-        }
+        const data = await this.userService.findMany(role)
+        return res.status(HttpStatus.OK).json(data)
     }
 
     @Get(':id')
     async findOne(@Param('id') id: string, @Res() res: Response) {
-        try {
-            console.log(`received: ${id}`)
-            const data =  await this.userService.findOne(id)
-            
-            return res.status(HttpStatus.OK).json(data)
-        }
-        catch(e) {
-            return handleUserResponseException(res, e)
-        }
+        const data =  await this.userService.findOne(id)
+        return res.status(HttpStatus.OK).json(data)
     }
 }
