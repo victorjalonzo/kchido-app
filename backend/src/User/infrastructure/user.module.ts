b1622834e4d0edb43138c0fd6a76ffc38ed80c8c
VJA <victorj.alonzo@gmail.com>
2025-05-11 21:53:00 -0400

    feat(user) add Permission module

--- backend/src/User/infrastructure/user.module.ts
+++ backend/src/User/infrastructure/user.module.ts
@@ -2,9 +2,10 @@ import { Module} from "@nestjs/common"
 import { UserService } from "../application/user.service"
 import { UserController } from "./user.controller"
 import { SharedModule } from "../../Shared/shared.module"
+import { PermissionService } from "src/Permission/application/permission.service"
 
 @Module({
-    imports: [SharedModule],
+    imports: [SharedModule, PermissionService],
     providers: [UserService],
     controllers: [UserController]
 })
