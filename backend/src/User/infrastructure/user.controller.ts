a4a4c54cc37aa4cab3bea5347e87ecb4be6b2820
VJA <victorj.alonzo@gmail.com>
2025-05-11 21:48:13 -0400

    feat(user) add handlers for user creation/deletion

--- backend/src/User/infrastructure/user.controller.ts
+++ backend/src/User/infrastructure/user.controller.ts
@@ -1,12 +1,20 @@
-import {Controller, Get, Param, Res, HttpStatus, Query, UseFilters} from "@nestjs/common"
-import { UserService } from "../application/user.service.js"
+import {Controller, Get, Post, Param, UsePipes, ValidationPipe, Res, HttpStatus, Query, UseFilters, Body, Delete} from "@nestjs/common"
+import { UserService } from "../application/user.service"
 import { Response } from 'express';
-import { UserExceptionFilter } from "./user-exception.filter.js";
+import { UserExceptionFilter } from "./user-exception.filter";
+import { CreateUserDTO } from "../application/create-user.dto";
 
 @UseFilters(UserExceptionFilter)
 @Controller("api/v1/users")
 export class UserController {
     constructor(private readonly userService: UserService){}
+
+    @Post()
+    @UsePipes(new ValidationPipe())
+    async create(@Body() createUserDto: CreateUserDTO, @Res() res: Response) {
+        const data = await this.userService.create(createUserDto)
+        return res.status(HttpStatus.OK).json(data)
+    }
 
     @Get()
     async findMany(@Query('role') role: string, @Res() res: Response) {
@@ -17,6 +25,12 @@ export class UserController {
     @Get(':id')
     async findOne(@Param('id') id: string, @Res() res: Response) {
         const data =  await this.userService.findOne(id)
+        return res.status(HttpStatus.OK).json(data)
+    }
+
+    @Delete()
+    async delete(@Param('id') id: string, @Res() res: Response) {
+        const data = await this.userService.delete(id)
         return res.status(HttpStatus.OK).json(data)
     }
 }
