08a4f2acc8900f56175122a18219ca83a33de93b
VJA <victorj.alonzo@gmail.com>
2025-05-19 23:30:55 -0400

    refactor (raffle): refactor service to avoid logic duplication in raffle.service.ts

--- backend/src/Raffle/application/raffle.service.ts
+++ backend/src/Raffle/application/raffle.service.ts
@@ -5,6 +5,7 @@ import { Raffle } from "../domain/raffle.entity";
 import { RaffleMapper } from "../infrastructure/raffle.mapper";
 import { CreateRaffleDTO } from "./create-raffle.dto";
 import { Injectable } from "@nestjs/common";
+import { RaffleStatus } from "../domain/raffle.entity";
 
 @Injectable()
 export class RaffleService {
@@ -17,21 +18,20 @@ export class RaffleService {
         .then(record => RaffleMapper.toDomain(record))
     }
 
+    findPublicOne = async (id: string) => {
+        return await this._findOne({ id, status: RaffleStatus.PUBLIC})
+    }
+
+    findPublicMany = async () => {
+        return await this._findMany({ status: RaffleStatus.PUBLIC})
+    }
+
     findOne = async (id: string): Promise<Raffle> => {
-        return await this.repository.findOne(this.model, { id })
-        .then(record => {
-            if (!record) throw new RaffleNotFoundException()
-            return RaffleMapper.toDomain(record)
-        })
+        return await this._findOne({ id })
     }
 
     findMany = async (): Promise<Raffle[]> => {
-        return await this.repository.findMany(this.model)
-        .then(records => {
-            return records.map(record => {
-                return RaffleMapper.toDomain(record)
-            })
-        })
+        return await this._findMany()
     }
 
     delete = async (id: string): Promise<Raffle> => {
@@ -41,4 +41,21 @@ export class RaffleService {
             return RaffleMapper.toDomain(record)
         })
     }
+
+    _findOne = async (filters: Record<string, any>): Promise<Raffle> => {
+        return await this.repository.findOne(this.model, filters)
+        .then(record => {
+            if (!record) throw new RaffleNotFoundException()
+            return RaffleMapper.toDomain(record)
+        })
+    }
+
+    _findMany = async (filters?: Record<string, any>): Promise<Raffle[]> => {
+        return await this.repository.findMany(this.model, filters)
+        .then(records => {
+            return records.map(record => {
+                return RaffleMapper.toDomain(record)
+            })
+        })
+    } 
 }
