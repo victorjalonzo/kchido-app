2fce9ae76fedba30b35f7250b892a5e01f96dded
VJA <victorj.alonzo@gmail.com>
2025-05-22 23:41:28 -0400

    feat(main) add morgan to watch HTTP logs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SharedConfig } from './Shared/shared.config';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  app.enableCors({origin: true, credentials: true})
+
+  app.use(morgan('dev'))
+
+  await app.listen(SharedConfig.serverPort);
 }
 bootstrap();
