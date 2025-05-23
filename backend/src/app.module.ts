244ff538d2272062222c1c65bb63987fa04333de
VJA <victorj.alonzo@gmail.com>
2025-05-22 23:40:39 -0400

    feat(app) add auth and raffle module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,13 +3,15 @@ import { UserModule } from './User/infrastructure/user.module';
 import { SharedModule } from './Shared/shared.module';
 import { RaffleModule } from './Raffle/infrastructure/raffle.module';
 import { PermissionModule } from './Permission/infrastructure/permission.module';
+import { AuthModule } from './Auth/infrastructure/auth.module';
 
 @Module({
   imports: [
     SharedModule, 
     UserModule,
     PermissionModule,
-    RaffleModule
+    RaffleModule,
+    AuthModule
   ],
 })
 export class AppModule {}
