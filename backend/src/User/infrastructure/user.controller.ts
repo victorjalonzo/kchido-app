6161b39ae1a13033b42d0c257656cf03afe1bd57
VJA <victorj.alonzo@gmail.com>
2025-05-22 23:38:24 -0400

    feat(user) add method to update and delete users in user.controller.ts

--- backend/src/User/infrastructure/user.controller.ts
+++ backend/src/User/infrastructure/user.controller.ts
@@ -1,8 +1,9 @@
-import {Controller, Get, Post, Param, UsePipes, ValidationPipe, Res, HttpStatus, Query, UseFilters, Body, Delete} from "@nestjs/common"
+import {Controller, Get, Post, Param, UsePipes, ValidationPipe, Res, HttpStatus, Query, UseFilters, Body, Delete, Put} from "@nestjs/common"
 import { UserService } from "../application/user.service"
 import { Response } from 'express';
 import { UserExceptionFilter } from "./user-exception.filter";
 import { CreateUserDTO } from "../application/create-user.dto";
+import { UpdateUserDTO } from "../application/update-user.dto";
 
 @UseFilters(UserExceptionFilter)
 @Controller("api/v1/users")
@@ -14,6 +15,12 @@ export class UserController {
     async create(@Body() createUserDto: CreateUserDTO, @Res() res: Response) {
         const data = await this.userService.create(createUserDto)
         return res.status(HttpStatus.OK).json(data)
+    }
+
+    @Put()
+    @UsePipes(new ValidationPipe())
+    async update(@Body() updateUserDto: UpdateUserDTO){
+        return await this.userService.update(updateUserDto)
     }
 
     @Get()
@@ -28,9 +35,8 @@ export class UserController {
         return res.status(HttpStatus.OK).json(data)
     }
 
-    @Delete()
-    async delete(@Param('id') id: string, @Res() res: Response) {
-        const data = await this.userService.delete(id)
-        return res.status(HttpStatus.OK).json(data)
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+        return await this.userService.delete(id)
     }
 }
