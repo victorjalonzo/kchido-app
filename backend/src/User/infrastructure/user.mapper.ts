0ea34345a6b8664a52feffc2c7dc97b1fbaabbfe
VJA <victorj.alonzo@gmail.com>
2025-05-22 23:37:30 -0400

    feat(user) add status to the mapper

--- backend/src/User/infrastructure/user.mapper.ts
+++ backend/src/User/infrastructure/user.mapper.ts
@@ -1,15 +1,17 @@
 import { Users } from "@prisma/client";
-import { User, UserType } from "../domain/user.entity";
+import { User, UserStatus, UserType } from "../domain/user.entity";
 
 export class UserMapper {
     static toDomain = (raw: Users) => {
         const role = <UserType>raw.role
+        const status = <UserStatus>raw.status
         const permissions = null
 
         return new User({
             ...raw,
             permissions,
-            role 
+            role, 
+            status
         })
     }
 }
