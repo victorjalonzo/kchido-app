1f0a15d8fd0cbeddc2c2fe076e38e20ef34c3d4e
VJA <victorj.alonzo@gmail.com>
2025-05-25 23:45:13 -0400

    feat(ticket) update _findMany method to accept filters an joins in ticket.service.ts

--- backend/src/Ticket/application/ticket.service.ts
+++ backend/src/Ticket/application/ticket.service.ts
@@ -6,6 +6,9 @@ import { Model } from "src/Shared/shared.types";
 import { TicketMapper } from "../infrastructure/ticket.mapper";
 import { TicketNotFound } from "../domain/ticket.exception";
 import { Injectable } from "@nestjs/common";
+import { FindTicketFilter } from "./find-ticket-filter";
+import { TicketJoinOption } from "./ticket-join-option";
+
 
 @Injectable()
 export class TicketService {
@@ -26,12 +29,10 @@ export class TicketService {
         })
     }
 
-    findMany = async (raffleId?: string) => {
-        return await this.repository.findMany(this.model, { raffleId })
-        .then(records => {
-            return records.map((record) => TicketMapper.toDomain(record))
-        })
+    findMany = async (filters?: FindTicketFilter, include?: TicketJoinOption): Promise<Ticket[]> => {
+        return await this._findMany(filters, include)
     }
+
 
     delete = async(id: string) => {
         return await this.repository.delete(this.model, { id })
@@ -41,4 +42,16 @@ export class TicketService {
         })
     }
 
+    _findMany = async (filters?: FindTicketFilter, include?: TicketJoinOption): Promise<Ticket[]> => {
+        return await this.repository.findMany(this.model, filters, include)
+        .then( records => records.map(record => TicketMapper.toDomain(record)))
+    }
+
+    _find = async (filters: Record<string, any>): Promise<Ticket> => {
+        return await this.repository.findOne(this.model, filters)
+        .then( record => {
+            if (!record) throw new TicketNotFound()
+            return TicketMapper.toDomain(record)
+        })
+    }
 }
