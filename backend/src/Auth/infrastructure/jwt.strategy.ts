f21e5b4b402501e9740682e3b095a28d3f458dde
VJA <victorj.alonzo@gmail.com>
2025-06-10 23:39:15 -0400

    refactor(auth) delete a comment

--- backend/src/Auth/infrastructure/jwt.strategy.ts
+++ backend/src/Auth/infrastructure/jwt.strategy.ts
@@ -12,7 +12,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    // Esto es lo que se añade al `req.user`
     return { userId: payload.sub };
   }
 }
