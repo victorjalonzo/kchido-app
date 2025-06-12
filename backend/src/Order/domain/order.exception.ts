232a6517cdf36f28a23b6eb762b33bfa6b3132cb
VJA <victorj.alonzo@gmail.com>
2025-06-11 23:44:04 -0400

    feat(order) add OrderStatusAlreadySet exception to trigger when a order already has a status set.

--- backend/src/Order/domain/order.exception.ts
+++ backend/src/Order/domain/order.exception.ts
@@ -1,5 +1,13 @@
+import { Order } from "./order.entity"
+
 export class OrderNotFound extends Error {
     constructor (){
         super('Order not found')
+    }
+}
+
+export class OrderStatusAlreadySet extends Error {
+    constructor(order: Order) {
+        super(`Order ${order.id} already has a status set: ${order.status}`)
     }
 }
