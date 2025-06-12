7c8866d9a0242ff856e9c952f76459719eb2fc23
VJA <victorj.alonzo@gmail.com>
2025-06-11 23:45:05 -0400

    feat(order) add properties to dto

--- backend/src/Order/application/create-order.dto.ts
+++ backend/src/Order/application/create-order.dto.ts
@@ -1,12 +1,27 @@
-import { IsNumber, IsString } from "class-validator"
+import { IsArray, IsNumber, IsOptional, IsString } from "class-validator"
 
 export class CreateOrderDTO {
     @IsString()
-    userId: string
+    raffleId: string 
 
-    @IsNumber()
-    total: number
+    @IsArray()
+    tickets: string[]
 
     @IsString()
+    userId: string
+    
+    @IsString()
     paymentMethod: string
+
+    @IsOptional() @IsString()
+    status: string 
+    
+    @IsOptional() @IsNumber()
+    total?: number
+
+    @IsOptional() @IsNumber()
+    quantity?: number
+
+    @IsOptional() @IsString()
+    assistedBy?: string 
 }
