999621fb15f568628acbf04e1832723c489d2f41
VJA <victorj.alonzo@gmail.com>
2025-06-14 21:28:43 -0400

    feat(order) add order controller

---
+++ backend/src/Order/infrastructure/order.controller.ts
@@ -0,0 +1,62 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req, Res, UseFilters, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
+import { CreateOrderDTO } from "../application/create-order.dto";
+import { OrderService } from "../application/order.service";
+import { UpdateOrderDTO } from "../application/update-order.dto";
+import { IncludeOrderQuery } from "../application/include-order.query";
+import { FindOrderQuery } from "../application/find-order.query";
+import { FindOrderDto } from "../application/find-order.dto";
+import { OrderExceptionFilter } from "./order-exception.filter";
+import { Request } from "express";
+import { JwtAuthGuard } from "src/Auth/infrastructure/jwt-auth.guard";
+import { QueryRequestExtractor } from "src/Shared/util/queries-extractor";
+
+@UseFilters(OrderExceptionFilter)
+@Controller('api/v1/orders')
+export class OrderController {
+    validFilters = ['userId', 'raffleId', 'status']
+    validIncludes = ['user', 'raffle', 'tickets', 'ticketReservations']
+
+    constructor(private readonly service: OrderService){}
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    @UsePipes(new ValidationPipe())
+    async create(@Body() createOrderDto: CreateOrderDTO, @Req() req: Request){
+        createOrderDto.assistedBy = req.user.userId
+        return await this.service.create(createOrderDto)
+    }
+
+    @Put()
+    @UsePipes(new ValidationPipe())
+    async update(@Body() updateOrderDto: UpdateOrderDTO){
+        return await this.service.update(updateOrderDto)
+    }
+
+    @Get()
+    @UsePipes(new ValidationPipe({transform: true}))
+    async findMany(@Query() query: FindOrderDto){
+        const { filterQueries, includeQueries } = QueryRequestExtractor.extract(query, {
+            validFilters: this.validFilters,
+            validIncludes: this.validIncludes
+        })
+
+        return await this.service.findMany(filterQueries, includeQueries)
+    }
+
+    @Get(':id')
+    @UsePipes(new ValidationPipe({transform: true}))
+    async findOne(@Param('id') id: string, @Query() query: FindOrderDto){
+        const { includeQueries } = QueryRequestExtractor.extract(query, {
+            validFilters: this.validFilters,
+            validIncludes: this.validIncludes
+        })
+
+        return await this.service.findById(id, includeQueries)
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string){
+        return await this.service.delete(id)
+    }
+    
+}
