40afdd675e7cb7fb5b238d8dc04e3c4f7b2ff31c
VJA <victorj.alonzo@gmail.com>
2025-06-15 23:35:41 -0400

    feat(payment) add taskModule

--- backend/src/Payment/infrastructure/payment.module.ts
+++ backend/src/Payment/infrastructure/payment.module.ts
@@ -4,9 +4,14 @@ import { PaymentProviderController } from "./payment-provider.controller";
 import { PaymentWebhookController } from "./payment-webhook.controller";
 import { PaymentProviderService } from "../application/payment-provider.service";
 import { OrderModule } from "src/Order/infrastructure/order.module";
+import { TaskModule } from "src/Task/infrastructure/task.module";
 
 @Module({
-    imports: [SharedModule, OrderModule],
+    imports: [
+        SharedModule, 
+        OrderModule, 
+        TaskModule
+    ],
     providers: [PaymentProviderService],
     controllers: [
         PaymentProviderController,
