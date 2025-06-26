2309b3fa8e275976da648575a21f8579f8df6539
VJA <victorj.alonzo@gmail.com>
2025-06-25 23:48:59 -0400

    feat(Raffle) add tickets, orders, participants and creator fields to be mapped.

--- backend/src/Raffle/infrastructure/raffle.mapper.ts
+++ backend/src/Raffle/infrastructure/raffle.mapper.ts
@@ -1,15 +1,28 @@
-import { Raffles } from "@prisma/client"
+import { Raffles as PrismaRaffle} from "@prisma/client"
 import { Raffle, RaffleStatus, RaffleVisibility } from "../domain/raffle.entity"
+import { RaffleIncludeValues } from "../application/raffle.service"
+import { TicketMapper } from "src/Ticket/infrastructure/ticket.mapper"
+import { OrderMapper } from "src/Order/infrastructure/order.mapper"
+import { UserMapper } from "src/User/infrastructure/user.mapper"
 
 export class RaffleMapper {
-    static toDomain = (raw: Raffles)  => {
+    static toDomain = (raw: (PrismaRaffle & RaffleIncludeValues))  => {
         const status = raw.status as RaffleStatus
         const visibility = raw.visibility as RaffleVisibility
+
+        const tickets = raw.tickets ? raw.tickets.map(ticket => TicketMapper.toDomain(ticket)) : undefined
+        const orders = raw.orders ? raw.orders.map(order => OrderMapper.toDomain(order)) : undefined
+        const participants = raw.participants ? raw.participants.map(participant => UserMapper.toDomain(participant)) : undefined
+        const creator = raw.creator ? UserMapper.toDomain(raw.creator) : undefined
  
         return new Raffle({
             ...raw,
             status,
-            visibility
+            visibility,
+            tickets,
+            orders,
+            participants,
+            creator
         })
     }
 }
