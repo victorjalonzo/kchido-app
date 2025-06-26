f4a95b93c3e1e653e4cd9f7029dbd5a1c89102ab
VJA <victorj.alonzo@gmail.com>
2025-06-25 23:46:04 -0400

    feat(Raffle) add creator, participants and winnerNumber fields and refactor createdBy for creatorId.

--- backend/src/Raffle/domain/raffle.entity.ts
+++ backend/src/Raffle/domain/raffle.entity.ts
@@ -1,5 +1,6 @@
 import { Order } from "src/Order/domain/order.entity"
 import { Ticket } from "src/Ticket/domain/ticket.entity"
+import { User } from "src/User/domain/user.entity"
 import { WinnerNumber } from "src/WinnerNumber/domain/winner-number.entity"
 
 export enum RaffleVisibility {
@@ -23,13 +24,16 @@ export interface Props {
   subscribers: number,
   status: RaffleStatus,
   visibility: RaffleVisibility,
-  winnerNumbers?: WinnerNumber[],
-  createdBy: string,
+  creatorId: string,
   createdAt: Date,
   endsAt: Date,
 
+  creator?: User
+  participants?: User[]
+  
   tickets?: Ticket[]
   orders?: Order[]
+  winnerNumbers?: WinnerNumber[],
 }
 
 export class Raffle implements Props {
@@ -43,13 +47,16 @@ export class Raffle implements Props {
   subscribers: number
   status: RaffleStatus
   visibility: RaffleVisibility
-  winnerNumbers: WinnerNumber[]
-  createdBy: string
+  creatorId: string
   createdAt: Date
   endsAt: Date
 
-  tickets?: Ticket[]
-  orders?: Order[]
+  creator?: User
+  participants: User[]
+  
+  tickets: Ticket[]
+  orders: Order[]
+  winnerNumbers: WinnerNumber[]
 
   constructor (props: Props){
     this.id = props.id
@@ -62,13 +69,16 @@ export class Raffle implements Props {
     this.subscribers = props.subscribers
     this.status = props.status
     this.visibility = props.visibility
-    this.winnerNumbers = props.winnerNumbers ?? []
-    this.createdBy = props.createdBy
+    this.creatorId = props.creatorId
     this.createdAt = props.createdAt
     this.endsAt = props.endsAt
 
+    this.creator = props.creator
+    this.participants = props.participants ?? []
+    
     this.tickets = props.tickets ?? []
     this.orders = props.orders ?? []
+    this.winnerNumbers = props.winnerNumbers ?? []
   }
 }
   
