510e45754bc48f8ac7c4ef34a3a9c518bba5eb39
VJA <victorj.alonzo@gmail.com>
2025-06-28 23:56:59 -0400

    refactor(user) refactor mapper to set the image value as a server url.

--- backend/src/User/infrastructure/user.mapper.ts
+++ backend/src/User/infrastructure/user.mapper.ts
@@ -1,17 +1,28 @@
 import { Users } from "@prisma/client";
 import { User, UserStatus, UserType } from "../domain/user.entity";
+import { IncludeUsersRelationValues } from "../application/user.service";
+import { PermissionMapper } from "src/Permission/infrastructure/permission.mapper";
+import { TicketMapper } from "src/Ticket/infrastructure/ticket.mapper";
+import { SharedConfig } from "src/Shared/shared.config";
 
 export class UserMapper {
-    static toDomain = (raw: Users) => {
+    static toDomain = (raw: (Users & IncludeUsersRelationValues)) => {
         const role = <UserType>raw.role
         const status = <UserStatus>raw.status
-        const permissions = null
+
+        const permissions = raw.permissions ? PermissionMapper.toDomain(raw.permissions) : null
+        const tickets = raw.tickets ? raw.tickets.map(ticket => TicketMapper.toDomain(ticket)) : []
+
+        if (raw.image) {
+            raw.image = `${SharedConfig.apiURL}/users/${raw.id}/profile`
+        }
 
         return new User({
             ...raw,
-            permissions,
             role, 
-            status
+            status,
+            permissions,
+            tickets
         })
     }
 }
