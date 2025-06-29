33b639fb004bf6fc2c61997b435246ef36bd7cf5
VJA <victorj.alonzo@gmail.com>
2025-06-29 12:36:36 -0400

    feat(ticket) add mappers for relation values

--- backend/src/Ticket/infrastructure/ticket.mapper.ts
+++ backend/src/Ticket/infrastructure/ticket.mapper.ts
@@ -1,8 +1,22 @@
 import { Tickets } from "@prisma/client";
 import { Ticket } from "../domain/ticket.entity";
+import { IncludeTicketsRelationValues } from "../application/ticket.service";
+import { UserMapper } from "src/User/infrastructure/user.mapper";
+import { RaffleMapper } from "src/Raffle/infrastructure/raffle.mapper";
+import { OrderMapper } from "src/Order/infrastructure/order.mapper";
 
 export class TicketMapper {
-    static toDomain(raw: Tickets): Ticket {
-        return new Ticket({...raw})
+    static toDomain(raw: Tickets & IncludeTicketsRelationValues): Ticket {
+        const user = raw.user ? UserMapper.toDomain(raw.user) : undefined
+        const raffle = raw.raffle ? RaffleMapper.toDomain(raw.raffle) : undefined
+        const order = raw.order ? OrderMapper.toDomain(raw.order) : undefined
+
+        return new Ticket({
+            ...raw,
+            receiptPath: <string>raw.receiptPath,
+            user,
+            raffle,
+            order
+        })
     }
 }
