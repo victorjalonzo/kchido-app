b52a698425dd0a786638b8532389c5bbabc8508b
VJA <victorj.alonzo@gmail.com>
2025-06-28 23:51:39 -0400

    feat(user) add new fields and getters to the entity

--- backend/src/User/domain/user.entity.ts
+++ backend/src/User/domain/user.entity.ts
@@ -1,9 +1,11 @@
 import { Permission } from "src/Permission/domain/permission.entity"
+import { Ticket } from "src/Ticket/domain/ticket.entity"
 
 export enum UserType {
     CUSTOMER = 'customer',
     SELLER = 'seller',
-    OWNER = 'owner'
+    ADMIN = 'admin',
+    CHATBOT = 'chatbot'
 }
 
 export enum UserStatus {
@@ -11,38 +13,75 @@ export enum UserStatus {
     BANNED = 'banned'
 }
 
-interface Props {
+interface UserProps {
     id: string
+    shortId: string
     name: string
+    image: string | null
     role: UserType
     number: string | null
+    contactNumber: string | null
     email: string | null
+    country: string | null
+    state: string | null
     password: string | null
     permissions: Permission | null
     status: UserStatus | null
     createdAt: Date
+
+    tickets?: Ticket[]
 }
 
-export class User implements Props {
+export class User implements UserProps {
     id: string
+    shortId: string
     name: string
+    image: string | null
     role: UserType
     number: string | null
+    contactNumber: string | null
     email: string | null
+    country: string | null
+    state: string | null
     password: string | null
     permissions: Permission | null
     status: UserStatus | null
     createdAt: Date
+    
+    tickets?: Ticket[]
 
-    constructor(props: Props) {
+    constructor(props: UserProps) {
         this.id = props.id
+        this.shortId = props.shortId
         this.name = props.name
+        this.image = props.image
         this.role = props.role
         this.number = props.number
+        this.contactNumber = props.contactNumber
         this.email = props.email
+        this.country = props.country
+        this.state = props.state
         this.password = props.password
         this.permissions = props.permissions
         this.status = props.status
         this.createdAt = props.createdAt
+        
+        this.tickets = props.tickets ?? []
+    }
+
+    get isAdmin(){
+        return this.role == UserType.ADMIN ? true : false
+    }
+
+    get isChatbot(){
+        return this.role == UserType.CHATBOT ? true : false
+    }
+
+    get isSeller(){
+        return this.role == UserType.SELLER ? true : false 
+    }
+
+    get isCustomer(){
+        return this.role == UserType.CUSTOMER ? true : false
     }
 }
