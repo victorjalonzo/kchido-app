8935756b73a73ff3a94a9d05278c5e846f859a3a
VJA <victorj.alonzo@gmail.com>
2025-06-29 12:45:12 -0400

    refactor(ticket) refactor module

--- backend/src/Ticket/infrastructure/ticket.module.ts
+++ backend/src/Ticket/infrastructure/ticket.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { SharedModule } from "src/Shared/shared.module";
 import { TicketService } from "../application/ticket.service";
+import { TicketController } from "./ticket.controller";
 
 @Module({
     imports: [SharedModule],
-    providers: [TicketService]
+    providers: [TicketService],
+    controllers: [TicketController],
+    exports: [TicketService]
 })
 export class TicketModule {}
