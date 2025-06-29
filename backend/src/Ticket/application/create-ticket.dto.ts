7f33a96d8b781f6db7f64c08d7594373c42432f0
VJA <victorj.alonzo@gmail.com>
2025-06-29 12:32:35 -0400

    feat(ticket) add shortId field to the CreateTicketDto

--- backend/src/Ticket/application/create-ticket.dto.ts
+++ backend/src/Ticket/application/create-ticket.dto.ts
@@ -1,9 +1,21 @@
-import { IsString } from "class-validator"
+import { IsNumber, IsOptional, IsString } from "class-validator"
 
 export class CreateTicketDTO {
     @IsString()
     serial: string
 
     @IsString()
-    raffleId: string 
+    raffleId: string
+
+    @IsString()
+    userId: string
+
+    @IsString()
+    orderId: string
+
+    @IsNumber()
+    price: number
+
+    @IsOptional() @IsString()
+    shortId?: string
 }
