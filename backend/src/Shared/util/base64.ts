eaba20abb34bc7410eebf1bd7405527b6143a735
VJA <victorj.alonzo@gmail.com>
2025-06-29 13:10:13 -0400

    refactor(shared) refactor util base64 to only return buffer and ext values.

--- backend/src/Shared/util/base64.ts
+++ backend/src/Shared/util/base64.ts
@@ -1,23 +1,12 @@
-// src/Shared/util/base64.ts
-import * as fs from 'fs';
-import * as path from 'path';
-
 export class Base64 {
-  static decode(base64String: string, filename: string) {
+  static decode(base64String: string) {
     const matches = base64String.match(/^data:(image\/\w+);base64,(.+)$/);
-    if (!matches) throw new Error('Formato base64 inválido');
+    if (!matches) throw new Error('Inválid format base64');
 
-    const ext  = matches[1].split('/')[1]; // png, jpeg…
+    const ext  = matches[1].split('/')[1];
     const data = matches[2];
     const buffer = Buffer.from(data, 'base64');
 
-    // usa una ruta absoluta para evitar sorpresas con el cwd
-    const uploadDir = path.resolve(process.cwd(), 'public');
-    if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
-
-    const filePath = path.join(uploadDir, `${filename}.${ext}`);
-    fs.writeFileSync(filePath, buffer);
-
-    return `${filename}.${ext}`;
+    return { buffer, ext }
   }
 }
