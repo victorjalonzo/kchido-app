2ec5012060da9bce5b84eb33f58eef3c0cb9c1ae
VJA <victorj.alonzo@gmail.com>
2025-06-29 13:24:57 -0400

    refactor(main) refactor bootstrap method.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SharedConfig } from './Shared/shared.config';
 import * as morgan from 'morgan';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
   app.enableCors({origin: true, credentials: true})
 
   app.use(morgan('dev'))
+  app.use(json({ limit: '10mb' }));
 
   await app.listen(SharedConfig.serverPort);
 }
