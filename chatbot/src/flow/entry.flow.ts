5ed865ac238ec0eb667d89f3544fcdf056d39d35
VJA <victorj.alonzo@gmail.com>
2025-06-30 21:11:38 -0400

    feat(flow) add banner as media to the entry message.

--- chatbot/src/flow/entry.flow.ts
+++ chatbot/src/flow/entry.flow.ts
@@ -2,6 +2,7 @@ import { addKeyword, EVENTS } from "@builderbot/bot";
 import { AuthAPI } from "../util/auth.api.js";
 import { menuFlow } from "./menu.flow.js";
 import { CustomerAPI } from "../util/customer.api.js";
+import { Asset } from "../util/asset.js";
 
 export const entryFlow = addKeyword(EVENTS.WELCOME)
 .addAction(async (ctx, {endFlow, state}) => {
@@ -27,6 +28,13 @@ export const entryFlow = addKeyword(EVENTS.WELCOME)
         console.log(`Something went wrong while trying to load configuration from entry point ${String(e)}`)
     }
 })
-.addAnswer('*Bienvenido a KChido* ☺️', {}, async (_, {gotoFlow}) => {
-    return await gotoFlow(menuFlow)
+.addAction(async (_, { flowDynamic, gotoFlow }) => {
+    const message = {
+        body: '*Bienvenido a KChido* ☺️',
+        media: Asset.getBanner()
+    }
+
+    await flowDynamic([ message ])
+
+    return gotoFlow(menuFlow)
 })
