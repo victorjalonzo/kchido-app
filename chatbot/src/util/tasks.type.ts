6f410ca6d71fa4c0daa34bc47720404331332fe1
VJA <victorj.alonzo@gmail.com>
2025-06-30 21:29:32 -0400

    feat(util) add fields and task payloads.

--- chatbot/src/util/tasks.type.ts
+++ chatbot/src/util/tasks.type.ts
@@ -1,3 +1,8 @@
+import { Order } from "./order.type"
+import { Raffle } from "./raffle.type"
+import { Ticket } from "./ticket.type"
+import { User } from "./user.types"
+
 export enum TaskStatus {
     DELIVERED = 'delivered',
     PENDING = 'pending',
@@ -5,10 +10,21 @@ export enum TaskStatus {
 }
 
 export enum TaskType {
-    PAYMENT = 'payment',          
-    REMINDER = 'reminder',        
-    ANNOUNCEMENT = 'announcement',
-    FOLLOW_UP = 'follow_up',
+    RAFFLE_ENDED = 'raffle_ended',
+    PAYMENT_COMPLETED = 'payment_completed',
+}
+
+export interface SubTask {
+    id: string
+    taskId: string
+    customerId: string
+    status: TaskStatus 
+    retries: number
+    ticketId?: string | null
+    createdAt: Date
+    updatedAt: Date
+
+    ticket?: Ticket
 }
 
 export interface Task {
@@ -23,4 +39,19 @@ export interface Task {
     customerId?: string | null
     orderId?: string | null
     raffleId?: string | null
+
+    subTasks?: SubTask[]
+    customer?: User | null
+    order?: Order | null
+    raffle?: Raffle | null
+}
+
+export class UpdateTaskPayload {
+    id: string
+    status: TaskStatus
+}
+
+export class UpdateSubTaskPayload {
+    id: string
+    status: TaskStatus
 }
