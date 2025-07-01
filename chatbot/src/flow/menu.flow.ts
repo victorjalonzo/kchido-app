86d274c2d7fc0d39e3e07892956f7157508a305c
VJA <victorj.alonzo@gmail.com>
2025-06-30 21:12:46 -0400

    feat(flow) complete the menu flow

--- chatbot/src/flow/menu.flow.ts
+++ chatbot/src/flow/menu.flow.ts
@@ -1,23 +1,46 @@
 import { addKeyword, EVENTS } from "@builderbot/bot";
 import { buyTicketFlow } from "./buy-ticket.flow.js";
+import { Validator } from "../util/message-validator.js";
+import { ConversactionAbort } from "../util/exceptions.js";
+import { faqFlow } from "./faq.flow.js";
 
 export const menuFlow = addKeyword(EVENTS.ACTION)
-.addAction(async (_, { flowDynamic }) => {
-    const message = '*¿En que te puedo ayudar?* 😁'
-    +'\n'
-    + `\n*1*. Comprar boletos de sorteos 🏆`
-    + `\n*2*. Ver preguntas frecuentes ❓`
+.addAction(async (_, { flowDynamic, state }) => {
+    const customer = state.get('customer');
 
-    await flowDynamic(message)
+    const customerName = customer
+      ? customer.name[0].toUpperCase() + customer.name.slice(1).toLowerCase()
+      : '';
+
+    const availableMenuOptions = [
+        { id: 1, label: 'Comprar boletos de sorteos 🏆', flow: buyTicketFlow},
+        { id: 2, label: 'Ver preguntas frecuentes ❓', flow: faqFlow}
+    ]
+
+    state.update( { availableMenuOptions })
+
+    const message = `*¿En que te puedo ayudar ${customerName}?* 😁`
+    +'\n'
+    + availableMenuOptions.map((option) => `\n*${option.id}.* ${option.label}`)
+
+    const hint = "\n"
+    + "\n*Opciones*\n"
+    + '\n_1️⃣ Selecciona *1* para comprar boletos_'
+    + '\n_2️⃣ Selecciona *2* para ver las preguntas frecuentes_'
+    + Validator.getCancelHint()
+
+     await flowDynamic(message + hint)
 
 })
-.addAction({ capture: true }, async (ctx, {flowDynamic, fallBack, gotoFlow}) => {
-    switch(ctx.body) {
-        case '1': return gotoFlow(buyTicketFlow)
-        case '2': return gotoFlow(buyTicketFlow)
-        
-        default:
-            await flowDynamic('Porfavor seleccione una opcion numerica valida.')
-            return fallBack()
+.addAction({ capture: true }, async (ctx, {fallBack, gotoFlow, state, endFlow }) => {
+    try {
+        const availableMenuOptions = state.get('availableMenuOptions')
+        const option = Validator.getOption(ctx.body, availableMenuOptions)
+
+        return gotoFlow(option.flow)
+    }
+    catch(e) {
+        if (e instanceof ConversactionAbort) return endFlow(e.message)
+        return fallBack(e.message)
     }
 })
