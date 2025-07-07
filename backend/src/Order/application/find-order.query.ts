74990bce3c80576571620bd8450dbe1af54ccfdc
VJA <victorj.alonzo@gmail.com>
2025-07-06 23:53:30 -0400

    refactor(Order) add field asistedBy

--- backend/src/Order/application/find-order.query.ts
+++ backend/src/Order/application/find-order.query.ts
@@ -1,7 +1,9 @@
 import { OrderStatus } from "../domain/order.entity"
 
 export interface FindOrderQuery {
+    id?: string
     userId?: string
     raffleId?: string
     status?: OrderStatus
+    assistedBy?: string
 }
