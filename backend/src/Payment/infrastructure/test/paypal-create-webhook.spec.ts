f8a9c55856b8bb7bbccc68cf7e7add64395b5365
VJA <victorj.alonzo@gmail.com>
2025-07-07 23:55:25 -0400

    feat(payment) add test for paypal service

---
+++ backend/src/Payment/infrastructure/test/paypal-create-webhook.spec.ts
@@ -0,0 +1,28 @@
+import { PaymentProviderService } from "src/Payment/application/payment-provider.service"
+import { createPaymentProviderTestingModule } from "./create-payment-provider-testing-module"
+import { CreatePaymentProviderDTO, PaymentProviderName } from "src/Payment/application/create-payment-provider.dto"
+import { PaypalService } from "src/Payment/application/paypal.service"
+
+describe('Paypal webhook creation', () => {
+    let paypalService: PaypalService
+
+    beforeAll(async () => {
+        const module = await createPaymentProviderTestingModule()
+        paypalService = module.get(PaypalService)
+    })
+
+    it('should create a Paypal webhook', async () => {
+        const payload: CreatePaymentProviderDTO = {
+            clientId: "ARsehQ41wz2flCNJN5dVdTyG27NYYK8lZcWjo-tQ6oK-TSyvY3j7rCCxkN9EVt3SqDdamaMohG3VeeGS",
+            clientSecret: "EKVXR3nGpN4Ve2eL4F--E1C0frR1xtUaQxv7aILWaYvdmwzBd2pN8-1V2BvRRhMQTs0kkZK7kRmtYJd5",
+            name: PaymentProviderName.PAYPAL
+        }
+        
+        const accessToken = await paypalService.getAccessToken(payload.clientId, payload.clientSecret)
+        const webhook = await paypalService.createWebhook(accessToken)
+        
+        console.log(webhook)
+
+        expect(webhook).toBeDefined()
+    })
+})
