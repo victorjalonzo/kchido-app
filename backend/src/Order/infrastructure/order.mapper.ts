be540cbd235c0f579b2c2b7bbfb1285d74157fb6
VJA <victorj.alonzo@gmail.com>
2025-07-09 22:01:44 -0400

    feat(order) add method toPublicDomain to convert records to public entities.

--- backend/src/Order/infrastructure/order.mapper.ts
+++ backend/src/Order/infrastructure/order.mapper.ts
@@ -9,6 +9,8 @@ import { RaffleMapper } from "src/Raffle/infrastructure/raffle.mapper";
 import { TicketMapper } from "src/Ticket/infrastructure/ticket.mapper";
 import { TicketReservation } from "src/TicketReservation/domain/ticket-reservation.entity";
 import { TicketReservationMapper } from "src/TicketReservation/infrastructure/ticke-reservation.mapper";
+import { PublicOrder } from "../domain/public-order.entity";
+import { PublicRaffle } from "src/Raffle/domain/public-raffle.entity";
 
 export class OrderMapper {
     static toDomain = (raw: Orders & IncludeOrdersRelationValues) => {
@@ -26,6 +28,24 @@ export class OrderMapper {
             ...raw,
             status: (raw.status as OrderStatus),
             user: user,
+            raffle,
+            tickets,
+            ticketReservations
+        })
+    }
+
+    static toPublicDomain = (raw: Orders & IncludeOrdersRelationValues) => {
+        let raffle: PublicRaffle | undefined = undefined
+        let tickets: Ticket[] | undefined = undefined
+        let ticketReservations: TicketReservation[] | undefined = undefined
+
+        if (raw.raffle) raffle = RaffleMapper.toPublicDomain(raw.raffle)
+        if (raw.tickets) tickets = raw.tickets.map(ticket => TicketMapper.toDomain(ticket))
+        if (raw.ticketReservations) ticketReservations = raw.ticketReservations.map(ticket => TicketReservationMapper.toDomain(ticket))
+
+        return new PublicOrder({
+            ...raw,
+            status: (raw.status as OrderStatus),
             raffle,
             tickets,
             ticketReservations
