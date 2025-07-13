56da87b87fab9d87b25b796b54491e971b70f2a3
VJA <victorj.alonzo@gmail.com>
2025-07-12 23:54:35 -0400

    refactor(ticket) update test for invoice creation.

--- backend/src/Ticket/infrastructure/test/create-ticket-invoice.spec.ts
+++ backend/src/Ticket/infrastructure/test/create-ticket-invoice.spec.ts
@@ -1,6 +1,7 @@
 import { TicketService } from "src/Ticket/application/ticket.service"
 import { createTicketModuleTesting } from "./create-ticket-testing-module"
 import { TicketQRCode } from "../util/ticket.qrcode"
+import { Ticket } from "src/Ticket/domain/ticket.entity"
 
 describe('Ticket invoice', () => {
     let service: TicketService
@@ -11,7 +12,7 @@ describe('Ticket invoice', () => {
     })
 
     it ('should create a ticket invoice', async () => {
-        const ticket = await service._find({})
+        const ticket = <Ticket>await service._find({id: "14a0fd79-a37b-4b86-8772-e13c3fe252ed"})
         console.log(ticket.serial)
         const ticketPath = await TicketQRCode.generate(ticket)
 
