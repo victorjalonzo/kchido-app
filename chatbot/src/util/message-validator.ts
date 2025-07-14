09a3fc7fe0a5fe4886c36ac92c2cce384fa16470
VJA <victorj.alonzo@gmail.com>
2025-07-14 10:36:45 -0400

    refactor(util) add method getTicketNumber to filter the ticket number.

--- chatbot/src/util/message-validator.ts
+++ chatbot/src/util/message-validator.ts
@@ -35,6 +35,26 @@ export class Validator {
         return number;
     }
 
+    static getTicketNumber(reply: string): number {
+        Validator._isCancel(reply)
+
+        const number = parseInt(reply)
+        if (isNaN(number)) {
+            throw new Error('Por favor, escribe un valor numérico de 6 dígitos.')
+        }
+    
+        const length = number.toString().length
+        if (length > 6) {
+            throw new Error('El número *no puede tener más de 6 dígitos*. Escribe un valor numérico de 6 dígitos.')
+        }
+    
+        if (length < 6) {
+            throw new Error('El número *no puede tener menos de 6 dígitos*. Escribe un valor numérico de 6 dígitos.')
+        }
+    
+        return number
+    }
+
     static _isCancel(reply: string) {
         if (reply.toLowerCase() == 'cancelar') throw new ConversactionAbort()
         return false
