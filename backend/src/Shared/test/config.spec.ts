2257083fb2368de09c24cd76b43bfee2a41c7559
VJA <victorj.alonzo@gmail.com>
2025-07-13 23:43:58 -0400

    refactor(shared) update test

--- backend/src/Shared/test/config.spec.ts
+++ backend/src/Shared/test/config.spec.ts
@@ -3,7 +3,7 @@ import { sharedConfig } from "../shared.config"
 describe('Config test', () => {
     it('should return values', () => {
         expect(sharedConfig.databaseURL).toBeDefined()
-        expect(sharedConfig.apiURL).toBeDefined()
+        expect(sharedConfig.appApiURL).toBeDefined()
         expect(sharedConfig.chatbotServerURL).toBeDefined()
         expect(sharedConfig.jwtSecret).toBeDefined()
         expect(sharedConfig.jwtExpiresIn).toBeDefined()
