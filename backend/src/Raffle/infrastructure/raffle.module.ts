2d450d9938903b084878da82f24974c2ba8ed28b
VJA <victorj.alonzo@gmail.com>
2025-07-13 23:42:32 -0400

    refactor(raffle) include Taskmodule to the imports

--- backend/src/Raffle/infrastructure/raffle.module.ts
+++ backend/src/Raffle/infrastructure/raffle.module.ts
@@ -3,9 +3,14 @@ import { SharedModule } from "../../Shared/shared.module";
 import { RaffleService } from "../application/raffle.service";
 import { RaffleController } from "./raffle.controller";
 import { WinnerNumberModule } from "src/WinnerNumber/infrastructure/winner-number.module";
+import { TaskModule } from "src/Task/infrastructure/task.module";
 
 @Module({
-    imports: [SharedModule, WinnerNumberModule],
+    imports: [
+        SharedModule, 
+        TaskModule, 
+        WinnerNumberModule
+    ],
     providers: [RaffleService],
     controllers: [RaffleController],
     exports: [RaffleService]
