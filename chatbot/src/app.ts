19e68aa17f60f5dc5f5e6f74594cb58e40f9e41a
VJA <victorj.alonzo@gmail.com>
2025-07-14 10:38:35 -0400

    refactor(main) update main file.

--- chatbot/src/app.ts
+++ chatbot/src/app.ts
@@ -1,58 +1,14 @@
-import { createBot, createProvider, createFlow } from '@builderbot/bot'
+import { createBot, createProvider } from '@builderbot/bot'
 import { MemoryDB as Database } from '@builderbot/bot'
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
-import { entryFlow } from './flow/entry.flow.js'
-import { menuFlow } from './flow/menu.flow.js'
-import { 
-    buyTicketFlow, 
-    _chooseRaffleFlow,
-    _chooseTicketAmountFlow,
-    _chooseTicketTypeFlow,
-    _chooseManualTicketFlow,
-    _chooseGeneratedTicketFlow,
-    _confirmChoosenTickets,
-    _provideFirstTimeCountry,
-    _provideFirstTimeCountryStateFlow,
-    _provideFirstTimeNameFlow,
-    _provideFirstTimeNumberFlow,
-    _provideManualContactNumber,
-    _providePersonalInformation,
-    _confirmPersonalInformation,
-    _resumenFlow,
-    _addGroupFlow,
-    _orderCompletedFlow
-
-} from './flow/buy-ticket.flow.js'
-import { Task } from './util/tasks.type.js'
+import { Task, TaskType } from './util/tasks.type.js'
 import { OrderAPI } from './util/order.api.js'
 import { TaskPoller } from './util/task-poller.js'
-import { faqFlow } from './flow/faq.flow.js'
+import { adapterFlow } from './flow/flow.js'
 
 const PORT = process.env.PORT ?? 3008
 
 const main = async () => {
-    const adapterFlow = createFlow([
-        entryFlow, 
-        menuFlow, 
-        buyTicketFlow, 
-        _chooseRaffleFlow,
-        _chooseTicketAmountFlow,
-        _chooseTicketTypeFlow,
-        _chooseGeneratedTicketFlow,
-        _chooseManualTicketFlow,
-        _confirmChoosenTickets,
-        _providePersonalInformation,
-        _provideFirstTimeCountry,
-        _provideFirstTimeCountryStateFlow,
-        _provideFirstTimeNameFlow,
-        _provideFirstTimeNumberFlow,
-        _confirmPersonalInformation,
-        _resumenFlow,
-        _orderCompletedFlow,
-        _addGroupFlow,
-        faqFlow
-    ])
-    
     const adapterProvider = createProvider(Provider, {
         writeMyself: 'host'
     })
@@ -74,11 +30,24 @@ const main = async () => {
                 const order = await OrderAPI.get(task.orderId, { user: true })
                 const customer = order.user
 
-                await bot.dispatch('ORDER_COMPLETED', {
+                const args = {
                     from: customer.number, 
                     name: customer.name,
-                    task: task
-                })
+                    task: task 
+                }
+
+                switch(task.type) {
+                    case TaskType.PAYMENT_COMPLETED:
+                        await bot.dispatch('ORDER_COMPLETED', args)
+                        break;
+
+                    case TaskType.RAFFLE_ENDED:
+                        await bot.dispatch('RAFFLE_ENDED', args)
+                        break
+
+                    default:
+                        throw new Error('unknown task type.')
+                }
 
                 res.writeHead(200, { 'Content-Type': 'application/json' })
                 return res.end(JSON.stringify({ status: 'ok' }))
