ab331bcbccefe7e3e6713497ba459a3f469dfc71
VJA <victorj.alonzo@gmail.com>
2025-07-13 23:46:10 -0400

    refactor(task) remove RaffleModule and OrderModule from the imports.

--- backend/src/Task/infrastructure/task.module.ts
+++ backend/src/Task/infrastructure/task.module.ts
@@ -1,20 +1,16 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { SharedModule } from "src/Shared/shared.module";
 import { TaskService } from "../application/task.service";
 import { TaskController } from "./task.controller";
 import { TaskDispatcher } from "../application/task.dispatcher";
 import { SubTaskService } from "../application/subtask.service";
-import { RaffleModule } from "src/Raffle/infrastructure/raffle.module";
 import { TicketModule } from "src/Ticket/infrastructure/ticket.module";
 import { SubTaskController } from "./subtask.controller";
-import { OrderModule } from "src/Order/infrastructure/order.module";
 
 @Module({
     imports: [
         SharedModule,
-        RaffleModule,
-        TicketModule,
-        OrderModule
+        TicketModule
     ],
     providers: [
         TaskService, 
